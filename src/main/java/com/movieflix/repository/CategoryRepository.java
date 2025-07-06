0c8c1070b147e28192561a8d35a97a086456df65
Vinicius Lgo <vinicruzlago@gmail.com>
2025-07-06 09:39:48 -0300

    feat: add CategoryRepository for CRUD operations on Category entity

--- src/main/java/com/movieflix/repository/CategoryRepository.java
+++ src/main/java/com/movieflix/repository/CategoryRepository.java
@@ -1,4 +1,10 @@
 package com.movieflix.repository;
 
-public interface CategoryRepository {
+import com.movieflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
 }
